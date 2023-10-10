e6b18cae70b1580a93e9516eebb83b0fbfbe3173
cphovo <cphovo@qq.com>
2023-10-10 15:44:05 +0800

    add get file info func

--- utils/file.go
+++ utils/file.go
@@ -1,7 +1,38 @@
 package utils
 
+import (
+	"io"
+	"os"
+	"time"
+)
+
 type FileInfo struct {
-	Name string
+	Name      string
+	Content   string
+	CreatedAt time.Time
 }
 
-// 获取文件信息（文件名，文件创建时间）
+// Function to get the file info of a given filepath
+func GetFileInfo(filepath string) (*FileInfo, error) {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	bytes, err := io.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	return &FileInfo{
+		Name:      fileInfo.Name(),
+		Content:   string(bytes),
+		CreatedAt: fileInfo.ModTime(),
+	}, nil
+}
